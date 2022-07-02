95cf0355baeee512cb7c9e01ee8e41c142e4ddf6
francoasc <francoascarate349@gmail.com>
2022-07-03 01:07:56 +0200

    update Home screen logic

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,8 +1,10 @@
 import { NavigationProp } from "@react-navigation/native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, FlatList } from "react-native";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import AddFeedModal from "../../components/AddFeedModal/AddFeedModal";
 import ArticleCard from "../../components/ArticleCard";
+import LoadingSpinner from "../../components/LoadingSpinner";
 import TopNavigationBar from "../../components/TopNavigationBar";
 import { fetchRssXML } from "../../redux/reducers/rssSlice";
 import styles from "./Home.styles";
@@ -13,16 +15,35 @@ interface Props {
 
 const Home: React.FC<Props> = ({ navigation }) => {
   const dispatch = useAppDispatch();
-  const { rssListCopy, rssList } = useAppSelector((state) => state.rss);
+  const { rssListCopy, rssList, rssListURLs } = useAppSelector(
+    (state) => state.rss
+  );
+  const [showAddNewFeed, setShowAddNewFeed] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    dispatch(fetchRssXML("http://www.xatakandroid.com/tag/feeds/rss2.xml"));
-    // dispatch(fetchRssXML("http://www.reddit.com/r/Bitcoin/.rss"));
-  }, []);
+    // If there are feed urls then we fetch them
+    if (rssListURLs?.length) {
+      rssListURLs.forEach((url) => {
+        dispatch(fetchRssXML(url));
+      });
+    }
+  }, [rssListURLs]);
+
+  useEffect(() => {
+    if (!rssListURLs?.length) setIsLoading(false);
+
+    if (rssListCopy?.length || rssList?.length) setIsLoading(false);
+  }, [rssList]);
+
+  if (isLoading) return <LoadingSpinner />;
 
   return (
     <View style={styles.container}>
-      <TopNavigationBar />
+      <TopNavigationBar
+        setShowAddNewFeed={setShowAddNewFeed}
+        showAddNewFeed={showAddNewFeed}
+      />
       {rssListCopy.length ? (
         <FlatList
           data={rssListCopy}
@@ -31,7 +52,7 @@ const Home: React.FC<Props> = ({ navigation }) => {
           )}
           keyExtractor={({ id }) => id}
         />
-      ) : rssList.length ? ( // If rssList has length means that there's data but the filter didn't find it
+      ) : rssList.length ? ( // If rssList has length and rssListCopy doesn't means that there's data but the filter didn't find it
         <View style={styles.noDataContainer}>
           <Text>No se encontraron feeds con ese título</Text>
         </View>
@@ -40,6 +61,10 @@ const Home: React.FC<Props> = ({ navigation }) => {
           <Text>No hay feeds agregados!</Text>
         </View>
       )}
+      <AddFeedModal
+        showAddNewFeed={showAddNewFeed}
+        setShowAddNewFeed={setShowAddNewFeed}
+      />
     </View>
   );
 };
