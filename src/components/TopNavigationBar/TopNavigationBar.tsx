f004f681cee53ff554f3d179b8d8350861369146
francoasc <francoascarate349@gmail.com>
2022-07-02 21:05:11 +0200

    add TextInput for search articles by titles

--- src/components/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/TopNavigationBar/TopNavigationBar.tsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import styles from "./TopNavigationBar.styles";
 import { Entypo } from "@expo/vector-icons";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { sortRssList, RssOrder } from "../../redux/reducers/rssSlice";
+import {
+  sortRssList,
+  RssOrder,
+  filterByTitle,
+} from "../../redux/reducers/rssSlice";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TextInput } from "react-native-gesture-handler";
 
 const TopBar = () => {
-  const { order } = useAppSelector(({ rss }) => rss);
   const dispatch = useAppDispatch();
+  const { order } = useAppSelector(({ rss }) => rss);
 
   const handleReOrderArticles = () => {
     dispatch(sortRssList(order));
   };
+
   const handleAddNewFeed = () => {};
+
+  const handleSearchOnChange = (text: string) => {
+    dispatch(filterByTitle(text));
+  };
 
   return (
     <View style={styles.container}>
@@ -32,7 +42,12 @@ const TopBar = () => {
           />
         )}
       </TouchableOpacity>
-      <Text>Home</Text>
+      <TextInput
+        placeholder="Busca por título"
+        onChangeText={handleSearchOnChange}
+        style={styles.textInput}
+      />
+
       <TouchableOpacity>
         <Entypo name="add-to-list" size={24} color="black" />
       </TouchableOpacity>
