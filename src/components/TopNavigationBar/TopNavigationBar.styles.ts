2797ba16639eee27874529c420b68ac40de64f20
francoasc <francoascarate349@gmail.com>
2022-07-02 17:36:21 +0200

    add TopNavigationBar component

---
+++ src/components/TopNavigationBar/TopNavigationBar.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    alignSelf: "stretch",
+    height: 52,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderBottomColor: "#555",
+    borderBottomWidth: 0.5,
+    marginLeft: 8,
+    marginRight: 8,
+  },
+});
