73d2eb5ce1a380263700e98fbe7c484616b7944d
Franco Ascarate <70088727+francoasc@users.noreply.github.com>
2022-07-03 22:47:01 +0200

    Offline mode (#10)
    * add app icon
    
    * Add redux toolkit (#1)
    
    * add redux store provider
    
    * add rss reducer and redux hooks
    
    * add navigation and home screen (#2)
    
    * List rss items (#3)
    
    * update home styles and logic
    
    * add  react native rss parser
    
    * add navigation logic
    
    * add Article card component
    
    * update store middleware
    
    * add fetchRssXML logic
    
    * Add Top navigation bar component and sort logic (#4)
    
    * add dates helper functions
    
    * add TopNavigationBar component
    
    * update rss reducer
    
    * Add ArticleDetail screen (#5)
    
    * remove unnecesary imports
    
    * update routeNames and stack navigator
    
    * add ArticleDetail screen
    
    * add ArticleDetail rendering logic
    
    * Add input to filter by article title (#6)
    
    * update Articles render logic
    
    * add redux logic of filter by title
    
    * add TextInput for search articles by titles
    
    * Feature add new rss feed (#7)
    
    * add LoadingSpinner component
    
    * add AddFeedModal component
    
    * add AddFeed logic in TopNavigationBar component
    
    * add new rssUrls initial state
    
    * update Home screen logic
    
    * Feature Offline mode (#9)
    
    * add async storage package
    
    * fix sort by date logic
    
    * minor redux logic update in AddFeedModal
    
    * update reducer logic to AsyncStorage logic
    
    * add AsyncStorage logic in Home screen
    
    * naming the app for download
    
    * update yarn.lock

--- src/redux/reducers/rssSlice.ts
+++ src/redux/reducers/rssSlice.ts
@@ -8,6 +8,7 @@ import {
   sortInAscendingOrderByDate,
   sortInDescendingOrderByDate,
 } from "../../utils/dates";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export interface RssResponse {
   title: string;
@@ -29,6 +30,7 @@ export interface RssState {
   rssListCopy: RssResponse[];
   rssListURLs: string[];
   order: RssOrder;
+  isAsyncStorageLoading: boolean;
 }
 
 const initialState: RssState = {
@@ -36,6 +38,7 @@ const initialState: RssState = {
   rssListCopy: [],
   rssListURLs: [],
   order: RssOrder.DESCENDING,
+  isAsyncStorageLoading: true,
 };
 
 export const errorImage =
@@ -45,8 +48,133 @@ export const fetchRssXML = createAsyncThunk(
   "rss/fetchRssXML",
   async (url: string) => {
     const response = await axios.get(url);
+    const imgRegex = /<img.*?src=['"](.*?)['"]/;
+    
+    const rssDataParsed = await rssParser.parse(response.data);
 
-    return rssParser.parse(response.data);
+    // Getting only the data that I need from the response
+    const rssListToStorage = rssDataParsed.items.map((item: any) => {
+      const description = item.description
+        ? item.description.trim()
+        : item.content;
+      const date = new Date(item.published);
+      const imageExists = imgRegex.exec(description);
+      return {
+        description,
+        id: item.id,
+        url: item.links[0].url,
+        date,
+        stringifiedDate: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+        title: item.title,
+        image: imageExists
+          ? addHttpToUrl(imgRegex.exec(description)![1])
+          : errorImage,
+      };
+    });
+
+    // Checking if there's data in AsyncStorage (rssList)
+    let rssListAsyncStorage = await AsyncStorage.getItem("rssList");
+
+    if (!rssListAsyncStorage) {
+      // Saving the rssList and rssListIds in case they don't exist
+      await Promise.all([
+        AsyncStorage.setItem(
+          "rssListIds",
+          JSON.stringify(rssListToStorage.map((item: any) => item.id))
+        ),
+        AsyncStorage.setItem("rssList", JSON.stringify(rssListToStorage)),
+      ]);
+      return rssListToStorage;
+    } else {
+      let rssListIds = await AsyncStorage.getItem("rssListIds");
+      const newRssList: any[] = [];
+      const newRssListIds: string[] = [];
+      if (rssListIds) rssListIds = JSON.parse(rssListIds);
+
+      rssListAsyncStorage = JSON.parse(rssListAsyncStorage);
+
+      rssListToStorage.forEach((item: any) => {
+        if (!rssListIds!.includes(item.id)) {
+          newRssList.push(item);
+          newRssListIds.push(item.id);
+        }
+      });
+
+      await Promise.all([
+        AsyncStorage.setItem(
+          "rssListIds",
+          JSON.stringify([...new Set([...rssListIds!, ...newRssListIds])])
+        ),
+        AsyncStorage.setItem(
+          "rssList",
+          JSON.stringify([...rssListAsyncStorage!, ...newRssList])
+        ),
+      ]);
+      return [...rssListAsyncStorage!, ...newRssList];
+    }
+  }
+);
+
+export const addNewFeedURL = createAsyncThunk(
+  "rss/addNewFeedURL",
+  async (newFeed: string) => {
+    let asyncStorageFeedURLs = await AsyncStorage.getItem("rssListURLs");
+
+    if (!asyncStorageFeedURLs) {
+      // If feedUrls don't exist in local storage then we save the first one
+      await AsyncStorage.setItem("rssListURLs", JSON.stringify([newFeed]));
+
+      return [newFeed];
+    } else {
+      asyncStorageFeedURLs = JSON.parse(asyncStorageFeedURLs);
+      // If feedUrls exist in local storage then we save the first one
+      if (!asyncStorageFeedURLs!.includes(newFeed)) {
+        await AsyncStorage.setItem(
+          "rssListURLs",
+          JSON.stringify([...asyncStorageFeedURLs!, newFeed])
+        );
+        return [...asyncStorageFeedURLs!, newFeed];
+      }
+    }
+  }
+);
+
+export const deleteFeedURL = createAsyncThunk(
+  "rss/deleteFeedURL",
+  async (feedUrlToDelete: string) => {
+    let asyncStorageFeedURLs = await AsyncStorage.getItem("rssListURLs");
+
+    if (asyncStorageFeedURLs)
+      asyncStorageFeedURLs = JSON.parse(asyncStorageFeedURLs);
+
+    if (Array.isArray(asyncStorageFeedURLs)) {
+      // @ts-ignore
+      const newFeedUrls = asyncStorageFeedURLs.filter(
+        (url: string) => url !== feedUrlToDelete
+      );
+
+      await AsyncStorage.setItem("rssListURLs", JSON.stringify(newFeedUrls));
+
+      return newFeedUrls;
+    }
+  }
+);
+
+export const loadDataFromAsyncStorage = createAsyncThunk(
+  "rss/loadDataFromAsyncStorage",
+  async () => {
+    let [rssListUrls, rssList] = await Promise.all([
+      AsyncStorage.getItem("rssListURLs"),
+      AsyncStorage.getItem("rssList"),
+    ]);
+
+    if (rssList) rssList = JSON.parse(rssList);
+    if (rssListUrls) rssListUrls = JSON.parse(rssListUrls);
+
+    return {
+      rssListUrls: rssListUrls ? rssListUrls : [],
+      rssList: rssList ? rssList : [],
+    };
   }
 );
 
@@ -77,39 +205,13 @@ export const rssSlice = createSlice({
         );
       }
     },
-    addNewFeed: (state, action: PayloadAction<string>) => {
-      if (!state.rssListURLs.includes(action.payload)) {
-        state.rssListURLs.push(action.payload);
-      }
-    },
-    deleteFeed: (state, action: PayloadAction<string>) => {
-      state.rssListURLs = state.rssListURLs.filter(
-        (url) => url !== action.payload
-      );
+    changeIsAsyncStorageLoading: (state, action: PayloadAction<boolean>) => {
+      state.isAsyncStorageLoading = action.payload;
     },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchRssXML.fulfilled, (state, action) => {
-      const imgRegex = /<img.*?src=['"](.*?)['"]/;
-
-      state.rssList = action.payload.items.map((item: any) => {
-        const description = item.description
-          ? item.description.trim()
-          : item.content;
-        const date = new Date(item.published);
-        const imageExists = imgRegex.exec(description);
-        return {
-          description,
-          id: item.id,
-          url: item.links[0].url,
-          date,
-          stringifiedDate: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
-          title: item.title,
-          image: imageExists
-            ? addHttpToUrl(imgRegex.exec(description)![1])
-            : errorImage,
-        };
-      });
+      state.rssList = action.payload;
 
       if (state.order === RssOrder.ASCENDING)
         state.rssList.sort(sortInAscendingOrderByDate);
@@ -119,10 +221,22 @@ export const rssSlice = createSlice({
 
       state.rssListCopy = [...state.rssList];
     });
+
+    builder.addCase(addNewFeedURL.fulfilled, (state, action) => {
+      if (Array.isArray(action.payload)) state.rssListURLs = action.payload;
+    });
+    builder.addCase(deleteFeedURL.fulfilled, (state, action) => {
+      if (Array.isArray(action.payload)) state.rssListURLs = action.payload;
+    });
+    builder.addCase(loadDataFromAsyncStorage.fulfilled, (state, action) => {
+      state.rssListURLs = action.payload.rssListUrls as string[];
+      state.rssList = action.payload.rssList as RssResponse[];
+      state.rssListCopy = [...state.rssList];
+    });
   },
 });
 
-export const { sortRssList, filterByTitle, addNewFeed, deleteFeed } =
+export const { sortRssList, filterByTitle, changeIsAsyncStorageLoading } =
   rssSlice.actions;
 
 export default rssSlice.reducer;
--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -5,9 +5,19 @@ interface DateType {
 export const sortInAscendingOrderByDate = (
   { date: dateA }: DateType,
   { date: dateB }: DateType
-) => dateB.getTime() - dateA.getTime();
+) => {
+  // Asking if date is string as when we stringified the entire rssList then the date became a string
+  if (typeof dateA === "string") dateA = new Date(dateA);
+  if (typeof dateB === "string") dateB = new Date(dateB);
+  return dateB.getTime() - dateA.getTime();
+};
 
 export const sortInDescendingOrderByDate = (
   { date: dateA }: DateType,
   { date: dateB }: DateType
-) => dateA.getTime() - dateB.getTime();
+) => {
+  // same as above here
+  if (typeof dateA === "string") dateA = new Date(dateA);
+  if (typeof dateB === "string") dateB = new Date(dateB);
+  return dateA.getTime() - dateB.getTime();
+};
